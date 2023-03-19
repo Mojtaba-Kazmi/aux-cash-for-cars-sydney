469b9bebd61f7712cda033bf1762a23d57a21119
Zuhal Adina <adinazuhal2@gmail.com>
2023-03-20 00:22:10 +0500

    added twitter meta tags to contact

--- pages/contact.js
+++ pages/contact.js
@@ -45,6 +45,10 @@ const Contact = ({ title, description, image, url, keyword, width, height }) => 
         <meta property="og:image" content={image} />
         <meta property="og:image:width" content={width} />
         <meta property="og:image:height" content={height} />
+        <meta name="twitter:card" content='summary' />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
       </Head>
       <Box
         sx={{
