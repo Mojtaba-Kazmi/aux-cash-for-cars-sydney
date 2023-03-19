5cfa01364080527147d4076430e3f09557cd537c
Zuhal Adina <adinazuhal2@gmail.com>
2023-03-19 15:42:03 +0500

    Updated about head

--- pages/about.js
+++ pages/about.js
@@ -1,33 +1,38 @@
 import AboutCompany from "@/components/AboutCompany";
 import Head from "next/head";
 
-const About = () => {
+export async function getServerSideProps() {
+  return {
+    props: {
+      title: "About - Aux Cash For Cars",
+      description:
+        "(AUX CAR FOR CASH Sydney) Has Been Pioneering The Metal Resource Recycling Industry In Sydney Region, Australia Since 2010 That Mostly Has Been Active In Car Removal Industry.",
+      image: "https://auxcashforcarsydney.com/assets/postImage/about-us.png",
+      url: `https://auxcashforcarsydney.com/about`,
+      keyword:
+        "cash for cars sydney, Car Removal Sydney, sell my old car",
+      width: 1200,
+      height: 630,
+    },
+  };
+}
+
+const About = ({ title, description, image, url, keyword, width, height }) => {
   return (
     <>
       <Head>
-        <title>About - Aux Cash For Cars</title>
-        <meta
-          name="description"
-          content="(AUX CAR FOR CASH Sydney) Has Been Pioneering The Metal Resource Recycling 
-              Industry In Sydney Region, Australia Since 2010 That Mostly Has 
-              Been Active In Car Removal Industry."
-        />
-        <meta
-          name="keywords"
-          content="cash for cars sydney, Car Removal Sydney,sell my old car,"
-        ></meta>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keyword} />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="canonical" href={"/about"} />
-
+        <link rel="canonical" href={url} />
         <meta name="robots" content="index, follow" />
-        {/* <meta property="og:image" content={pic_A_1} /> */}
-        <meta property="og:title" content="About - Aux Cash For Cars" />
-        <meta
-          property="og:description"
-          content="(AUX CAR FOR CASH Sydney) Has Been Pioneering The Metal Resource Recycling 
-              Industry In Sydney Region, Australia Since 2010 That Mostly Has 
-              Been Active In Car Removal Industry."
-        />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+        <meta property="og:image:width" content={width} />
+        <meta property="og:image:height" content={height} />
       </Head>
       <AboutCompany />
     </>
