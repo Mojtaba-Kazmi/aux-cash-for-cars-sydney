a5f5551d7bd89c4d51c9ffb3771ca07bab46b0f7
Zuhal Adina <adinazuhal2@gmail.com>
2023-03-20 00:24:47 +0500

    added twitter meta tags to services

--- pages/services.js
+++ pages/services.js
@@ -34,6 +34,10 @@ const Services = ({ title, description, image, url, keyword, width, height }) =>
         <meta property="og:image" content={image} />
         <meta property="og:image:width" content={width} />
         <meta property="og:image:height" content={height} />
+        <meta name="twitter:card" content='summary' />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
       </Head>
       <OurServices />
     </>
