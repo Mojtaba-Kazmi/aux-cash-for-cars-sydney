717f3c904bd3866ad4a700f1642815d61d945bca
Zuhal Adina <adinazuhal2@gmail.com>
2023-03-19 16:49:08 +0500

    Updated head of services

--- pages/services.js
+++ pages/services.js
@@ -1,32 +1,39 @@
 import OurServices from "@/components/OurServices";
 import Head from "next/head";
 
-const Services = () => {
+export async function getServerSideProps() {
+  return {
+    props: {
+      title: "Services - Aux Cash For Cars Sydney",
+      description:
+        "Sell your car now for cash or a bank cheque. Aux Cash For Car & Aux Car Removals Pays The Highest Cash For Cars In Sydney, we Pay the top amount  for your old, unwanted, scrap, or unroadworthy cars",
+      image: "https://auxcashforcarsydney.com/assets/servises.png",
+      url: `https://auxcashforcarsydney.com/services`,
+      keyword:
+        "cash for cars sydney, Car Removal Sydney, sell my old car",
+      width: 1200,
+      height: 630,
+    },
+  };
+}
+
+const Services = ({ title, description, image, url, keyword, width, height }) => {
   return (
     <>
       <Head>
-        <title>Aux Cash For Cars Sydney & Car Removals</title>
-        <meta
-          name="description"
-          content="Sell your car now for cash or a bank cheque. Aux Cash For Car & Aux Car Removals Pays The Highest Cash For Cars In Sydney, we Pay the top amount  for your old, unwanted, scrap, or unroadworthy cars"
-        />
-        <meta
-          name="keywords"
-          content="cash for cars sydney, Car Removal Sydney,sell my old car,"
-        ></meta>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keyword} />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="canonical" href={"/services"} />
-
+        <link rel="canonical" href={url} />
         <meta name="robots" content="index, follow" />
-        {/* <meta property='og:image' content={services_pic1} /> */}
-        <meta
-          property="og:title"
-          content="Aux Cash For Cars Sydney & Car Removals"
-        />
-        <meta
-          property="og:description"
-          content="Sell your car now for cash or a bank cheque. Aux Cash For Car & Aux Car Removals Pays The Highest Cash For Cars In Sydney, we Pay the top amount  for your old, unwanted, scrap, or unroadworthy cars"
-        />
+        <meta property="og:url" content={url} />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+        <meta property="og:image:width" content={width} />
+        <meta property="og:image:height" content={height} />
       </Head>
       <OurServices />
     </>
