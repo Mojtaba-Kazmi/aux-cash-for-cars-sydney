6a4bf8e8f4ab9e68b5628d504908295d1fc70603
Zuhal Adina <adinazuhal2@gmail.com>
2023-03-19 20:24:38 +0500

    Post1 update

--- pages/blog/post1.jsx
+++ pages/blog/post1.jsx
@@ -49,11 +49,12 @@ function post0({ title, description, image, url, keyword, width, height }) {
         <meta property="og:image" content={image} />
         <meta property="og:image:width" content={width} />
         <meta property="og:image:height" content={height} />
-        <meta name="twitter:card" content={description} />
+        
+        <meta name="twitter:card" content='summary' />
         <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content="This is an example website." />
+        <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={image} />
-        <meta name="twitter:site" content="@example" />
+        {/* <meta name="twitter:site" content="@example" /> */}
       </Head>
       <Container>
         <Box width={"100%"} overflow={"Hidden"}>
