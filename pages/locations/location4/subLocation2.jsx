309c046bc71d9d2a3d210fb0e8a867e380a5dfa9
hussaini <hussaini.aliahmad313@gmail.com>
2023-09-30 10:46:43 +0500

    location 4 sub 1,2,3 updated

--- pages/locations/location4/subLocation2.jsx
+++ pages/locations/location4/subLocation2.jsx
@@ -91,7 +91,7 @@ function post0({ title, description, image, url, keyword, width, height }) {
                   >
                     <Grid item md={6} sm={6} xs={2}>
                       <Link
-                        href="/locations/location3"
+                        href="/locations/location4"
                         style={{ width: "10px" }}
                       >
                         <NextLinkBtn>Back</NextLinkBtn>
