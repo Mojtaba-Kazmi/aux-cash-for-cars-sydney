312d100b96993a8795d02db484fbcc19fb9480a4
hussaini <hussaini.aliahmad313@gmail.com>
2023-12-02 04:52:45 +0500

    test 8

--- pages/faqs/question1.jsx
+++ pages/faqs/question1.jsx
@@ -9,7 +9,7 @@ import {
   Typography,
 } from "@mui/material";
 import Link from "next/link";
-import { subLocation } from "../api/faqs-content";
+import { question } from "./../api/faqs-content";
 import ContactForm from "@/components/ContactForm";
 import ContactUsCard from "@/components/ContactUsCard";
 import SocialShareBtn, { Line } from "@/components/GlobalComponents";
@@ -22,11 +22,11 @@ import { useRouter } from "next/router";
 export async function getServerSideProps() {
   return {
     props: {
-      title: "What is the process of selling a car for cash in Sydney?",
+      title: "Cash for Cars North Sydney",
       description:
-        "Discover a hassle-free way to sell your car for cash in Sydney! At Aux Cash for Cars, we simplify the process with a quick online valuation, on-site inspections, and instant cash offers. Our transparent and efficient system includes free car removal and environmentally friendly disposal. Experience a seamless transaction – turn your unwanted vehicle into instant cash today!",
-      image: "/assets/locationsImages/sublocation1Images/1.jpg",
-      url: `https://auxcashforcarsydney.com/faqs/question1`,
+        "Get top cash for cars in North Sydney with Aux Cash for Cars! We offer hassle-free vehicle selling and removal services, providing instant cash for your unwanted cars, trucks, and SUVs. Turn your old vehicles into cash quickly and conveniently. Contact us today for a seamless car selling experience!",
+      image: "/assets/locationsImages/sublocation1Images/30.jpg",
+      url: `https://auxcashforcarsydney.com/locations/location3/subLocation1`,
       keyword: "Cash for cars Sydney, car remval Sydney",
       width: 1200,
       height: 630,
@@ -71,13 +71,13 @@ function post0({ title, description, image, url, keyword, width, height }) {
                 <Card>
                   <CardMedia
                     sx={{ height: "300px" }}
-                    image={subLocation[1].image}
-                    title={subLocation[1].title}
-                    alt={subLocation[1].title}
+                    image={question[1].image}
+                    title={question[1].title}
+                    alt={question[1].title}
                   />
                   <CardContent>
-                    <h1>{subLocation[1].title}</h1>
-                    {subLocation[1].description}
+                    <h1>{question[1].title}</h1>
+                    {question[1].description}
                   </CardContent>
 
                   <Grid
@@ -90,7 +90,10 @@ function post0({ title, description, image, url, keyword, width, height }) {
                     }}
                   >
                     <Grid item md={6} sm={6} xs={2}>
-                      <Link href="/faqs" style={{ width: "10px" }}>
+                      <Link
+                        href="/locations/location3"
+                        style={{ width: "10px" }}
+                      >
                         <NextLinkBtn>Back</NextLinkBtn>
                       </Link>
                     </Grid>
@@ -114,7 +117,7 @@ function post0({ title, description, image, url, keyword, width, height }) {
                   <Card>
                     <CardContent>
                       <Typography component={"p"}> Tags </Typography>
-                      <TagsStyle>{subLocation[1].tags}</TagsStyle>
+                      <TagsStyle>{question[1].tags}</TagsStyle>
                     </CardContent>
                   </Card>
                 </Grid>
